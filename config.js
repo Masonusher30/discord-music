54a939134b997fb9f530dc131c951c29a9c5ca6c
Masonusher30 <87398430+Masonusher30@users.noreply.github.com>
2021-07-14 00:34:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "prefix": "/",
     "token_bot": "ODQyMTczODk4MjU0NTgxNzYw.YJxdkA.ZHF8bMvUfqan-Np9KoR88Y7ft6s",
-    "inviteURI": "https://discord.com/api/oauth2/authorize?client_id=842173898254581760&permissions=0&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize%3Fclient_id%3D842173898254581760%26permissions%3D0%26redirect_uri%3Dhttps%253A%252F%252Fdiscord.com%252Fapi%252Foauth2%252Fauthorize%253Fclient_id%253D842173&response_type=code&scope=identify%20bot%20applications.builds.read",
+    "inviteURI": "https://discord.com/api/oauth2/authorize?client_id=842173898254581760&permissions=0&redirect_uri=https%3A%2F%2Fdiscord.com%2Fapi%2Foauth2%2Fauthorize%3Fclient_id%3D842173898254581760%26perm",
     "supportServer": "supportServer", //if any
      "colors": {
       "error": "0xFF0000",
